7ff3b763156c6ac157d37b90f72145ec0a526e37
Arthur <121949480+ArthurAndradee@users.noreply.github.com>
2023-01-28 14:57:49 -0300

    Add files via upload

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { Trash } from "react-feather"
+import styles from "./styles.module.css"
+import { IComponentTaskProps } from "./types"
+
+ 
+export function Task({task, handleToggleTask, handleRemoveTask}: IComponentTaskProps) {
+  return (
+    <div className={styles.container}>
+      <input type="checkbox"
+      readOnly
+      checked={task.isCompleted}
+      className={styles.input}
+      onClick={() => handleToggleTask(task)}
+      />
+      <label className={task.isCompleted ? styles.complete: ""} >{task.title}</label>
+ 
+      <div className={styles.buttonsWrapper}>
+        <button type="button" className={styles.button}
+        onClick={() => { handleRemoveTask (task.id) }}>
+          <Trash size={16} />
+        </button>
+      </div>
+    </div>
+  )
+}
